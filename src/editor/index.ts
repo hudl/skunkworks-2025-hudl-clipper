93013b0967993b1bb4a50d23d8bff68ea9f67501
barret-fisher <105077709+barret-fisher@users.noreply.github.com>
2025-06-04 18:24:23 -0400

    fetching reports code

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -5,6 +5,9 @@ import "../../node_modules/cropnow/index.css";
 
 const Cropnow = cropLib.default;
 
+const token = import.meta.env.VITE_FASTMODEL_TOKEN;
+
+const selectReports = document.getElementById("reports")!;
 const btnShowCropCanvas = document.getElementById("btn-crop")!;
 const btnSaveClip = document.getElementById("btn-save-clip")!;
 const fileInput = document.getElementById("file-input")!;
@@ -15,6 +18,8 @@ const alertElement = document.querySelector(".alert")!;
 btnShowCropCanvas.style.display = "none";
 btnSaveClip.style.display = "none";
 
+let isDownload = true;
+
 let blobUrl: string;
 let cropper: any;
 let title: string;
@@ -22,6 +27,56 @@ let title: string;
 let imgUrl: any;
 let imgWidth;
 let imgHeight;
+
+async function getReports() {
+  const url = "https://fastreport-bg.fastmodelsports.com/1/report/";
+  try {
+    const response = await fetch(url,{
+        method: 'GET',
+        headers: new Headers({
+            'Authorization': 'Bearer '+ token,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        })
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    let report_response = Object.values(await response.json());
+
+    if (Array.isArray(report_response)) {
+      let reports = report_response[0];
+      if (Array.isArray(reports)) {
+        //Create and append the options
+        for (var i = 0; i < reports.length; i++) {
+            var option = document.createElement("option");
+            option.value = reports[i]["report_id"];
+            option.text = reports[i]["subtitle"];
+            selectReports.appendChild(option);
+        }
+      }
+    }
+
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getReports();
+
+function selectReport(event) {
+    var selectElement = event.target;
+    var value = selectElement.value;
+    if (isNaN(value) || value === "") {
+      isDownload = true;
+      btnSaveClip.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewbox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>download clip';
+    } else {
+      isDownload = false;
+      btnSaveClip.innerHTML = 'save clip to Fastscout';
+    }
+}
+
+selectReports.addEventListener("change", selectReport);
 
 chrome.runtime.sendMessage({ actionType: "get-screenshot" }, (response) => {
   const { imageBase64, tabTitle } = response;
---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+/// <reference types="vite/types/importMeta.d.ts" />
+
+interface ImportMetaEnv {
+  readonly VITE_FASTMODEL_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
